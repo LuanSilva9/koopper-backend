3a938b053c3b8d36b3c08f2f3b06eca14afd149b
LuanSilva9 <luanpdev@gmail.com>
2026-01-09 22:17:46 -0400

    origin: add initial files

---
+++ src/main/java/com/ekos/koopper/KoopperApplication.java
@@ -0,0 +1,13 @@
+package com.ekos.koopper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoopperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoopperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ekos/koopper/KoopperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ekos.koopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
